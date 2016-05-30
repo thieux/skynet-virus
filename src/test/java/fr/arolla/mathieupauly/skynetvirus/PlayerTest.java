8663dd35439abe220256894264038d9c8a897f50
Mathieu Pauly <mathieu.pauly@arolla.fr>
2016-05-30 09:47:59 +0200

    Makes pass the star acceptance test.

--- src/test/java/fr/arolla/mathieupauly/skynetvirus/PlayerTest.java
+++ src/test/java/fr/arolla/mathieupauly/skynetvirus/PlayerTest.java
@@ -65,4 +65,88 @@ public class PlayerTest {
         Assert.assertEquals("0 1\n2 3\n", output.toString());
     }
 
+    @Test
+    public void star() {
+        String input = "12\n" +
+                "23\n" +
+                "1\n" +
+                "11\n" +
+                "6\n" +
+                "0\n" +
+                "9\n" +
+                "1\n" +
+                "2\n" +
+                "0\n" +
+                "1\n" +
+                "10\n" +
+                "1\n" +
+                "11\n" +
+                "5\n" +
+                "2\n" +
+                "3\n" +
+                "4\n" +
+                "5\n" +
+                "8\n" +
+                "9\n" +
+                "6\n" +
+                "7\n" +
+                "7\n" +
+                "8\n" +
+                "0\n" +
+                "6\n" +
+                "3\n" +
+                "4\n" +
+                "0\n" +
+                "2\n" +
+                "11\n" +
+                "7\n" +
+                "0\n" +
+                "8\n" +
+                "0\n" +
+                "4\n" +
+                "9\n" +
+                "10\n" +
+                "0\n" +
+                "5\n" +
+                "0\n" +
+                "7\n" +
+                "0\n" +
+                "3\n" +
+                "0\n" +
+                "10\n" +
+                "5\n" +
+                "6\n" +
+                "0\n" +
+                "11\n" +
+                "6\n" +
+                "7\n" +
+                "8\n" +
+                "9\n" +
+                "10\n" +
+                "1\n" +
+                "2\n" +
+                "3\n" +
+                "4\n" +
+                "5\n";
+
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        new Player().play(
+                new ByteArrayInputStream(input.getBytes()),
+                new PrintStream(output));
+
+        Assert.assertEquals("11 5\n" +
+                        "6 0\n" +
+                        "7 0\n" +
+                        "8 0\n" +
+                        "9 0\n" +
+                        "10 0\n" +
+                        "1 0\n" +
+                        "2 0\n" +
+                        "3 0\n" +
+                        "4 0\n" +
+                        "5 0\n",
+                output.toString());
+    }
+
 }
