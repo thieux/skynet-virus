e0f84606b804c83fb1ab59a726945d44b0a90cd9
Mathieu Pauly <mathieu.pauly@arolla.fr>
2016-05-30 07:53:33 +0200

    Create an echo implementation of the virus program used to extract test inputs.

--- src/test/java/fr/arolla/mathieupauly/SkynetVirusTest.java
+++ src/test/java/fr/arolla/mathieupauly/SkynetVirusTest.java
@@ -1,11 +1,39 @@
 package fr.arolla.mathieupauly;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Scanner;
 
 public class SkynetVirusTest {
 
+    /**
+     * Echo implementation used on CodinGame to extract the standard input
+     */
+    static class Player {
+        public static void main(String args[]) {
+            Scanner in = new Scanner(System.in);
+            while (true) {
+                int N = in.nextInt();
+
+                System.err.println(N);
+            }
+        }
+    }
+
     @Test
-    public void canaryTest() {
+    public void simple() {
+        String input = "3\n" +
+                "2\n" +
+                "1\n" +
+                "1\n" +
+                "2\n" +
+                "1\n" +
+                "0\n" +
+                "2\n" +
+                "1";
+        String output = "0 1";
+        Assert.assertEquals("1 2", output);
     }
 
 }
